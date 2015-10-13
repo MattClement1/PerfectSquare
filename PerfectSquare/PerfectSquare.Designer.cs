7dfefe5ff3d834965f41e8b5185a30da60e0a433
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-10-13 13:11:38 -0400

    Made perfect square program.

---
+++ PerfectSquare/PerfectSquare.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace PerfectSquare
+{
+    partial class frmPerfectSquare
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSort = new System.Windows.Forms.Button();
+            this.nudNumber = new System.Windows.Forms.NumericUpDown();
+            this.lstPerfectSquares = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSort
+            // 
+            this.btnSort.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSort.Location = new System.Drawing.Point(65, 39);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 0;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // nudNumber
+            // 
+            this.nudNumber.Location = new System.Drawing.Point(42, 68);
+            this.nudNumber.Name = "nudNumber";
+            this.nudNumber.Size = new System.Drawing.Size(120, 20);
+            this.nudNumber.TabIndex = 1;
+            // 
+            // lstPerfectSquares
+            // 
+            this.lstPerfectSquares.FormattingEnabled = true;
+            this.lstPerfectSquares.Location = new System.Drawing.Point(42, 107);
+            this.lstPerfectSquares.Name = "lstPerfectSquares";
+            this.lstPerfectSquares.Size = new System.Drawing.Size(120, 108);
+            this.lstPerfectSquares.TabIndex = 2;
+            // 
+            // frmPerfectSquare
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(204, 229);
+            this.Controls.Add(this.lstPerfectSquares);
+            this.Controls.Add(this.nudNumber);
+            this.Controls.Add(this.btnSort);
+            this.Name = "frmPerfectSquare";
+            this.Text = "Perfect Squares";
+            ((System.ComponentModel.ISupportInitialize)(this.nudNumber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.NumericUpDown nudNumber;
+        private System.Windows.Forms.ListBox lstPerfectSquares;
+    }
+}
+
---
+++ PerfectSquare/PerfectSquare.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PerfectSquare
+{
+    public partial class frmPerfectSquare : Form
+    {
+        public frmPerfectSquare()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            int value = 1;
+            int endingValue;
+            int squareRootAsInteger;
+            int numberOfPerfectSquares = 0;
+            double squareRootAsDouble;
+
+            this.lstPerfectSquares.Items.Clear();
+
+            endingValue = Convert.ToInt32(this.nudNumber.Text);
+
+            while (value <= endingValue)
+            {
+                squareRootAsDouble = Math.Sqrt(value);
+                squareRootAsInteger = Convert.ToInt32(squareRootAsDouble);
+
+                if (squareRootAsInteger == squareRootAsDouble)
+                {
+                    numberOfPerfectSquares++;
+                    this.lstPerfectSquares.Items.Add(value);
+                }
+
+                value++;
+            }
+        }
+    }
+}
